825424e2ff104f85f1232c47a75736daa536218f
Eric Turner <ericturnerdev@gmail.com>
2016-09-19 16:50:48 +0900

    Add tabelog english url

--- app/controllers/v1/restaurants_controller.rb
+++ app/controllers/v1/restaurants_controller.rb
@@ -1,16 +1,22 @@
 module V1
   class RestaurantsController < ApplicationController
     def index
-      restaurants = if params[:area_id].present?
-                      Restaurant.by_area(params[:area_id])
-                    else
-                      Restaurant.all
-                    end
+      restaurants = find_restaurants
       render json: restaurants.page(params[:page])
     end
 
     def show
       render json: Restaurant.find(params[:id])
+    end
+
+    private
+
+    def find_restaurants
+      if params[:area_id].present?
+        Restaurant.by_area(params[:area_id])
+      else
+        Restaurant.all
+      end
     end
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,8 +5,21 @@ class Restaurant < ActiveRecord::Base
   has_many :genres, through: :genre_restaurants
 
   validates :name, presence: true, uniqueness: true
+  validates :url,
+            presence: true,
+            uniqueness: true,
+            format: { with: /tabelog\.com/ }
 
   def self.by_area(area_id)
     Area.find(area_id).restaurants
+  end
+
+  def tabelog_url
+    url
+  end
+
+  def tabelog_url_en
+    fail "no tabelog url" unless url
+    tabelog_url.sub("tabelog.com/", "tabelog.com/en/")
   end
 end
